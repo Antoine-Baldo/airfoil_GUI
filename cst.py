c30125fc123cb07fafaeedca07a430ab7460b48b
Antoine-Baldo <abaldo@tamu.edu>
2017-10-05 14:11:47 -0500

    Final

--- cst.py
+++ cst.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.patches as mpatches
 from matplotlib.figure import Figure
 import numpy
 from PyQt4 import QtGui, QtCore
@@ -27,7 +28,7 @@ class Window(QtGui.QDialog):
 
 		def CSTMOD():
 			chord = 1.
-			n = 200
+			n = 20
 			x = create_x(chord, n, distribution = 'polar')
 			ax = self.fig.add_subplot(111)
 
@@ -37,12 +38,6 @@ class Window(QtGui.QDialog):
 			V_Al1 = self.slAl1.value()
 			V_Deltasz = self.slDelta.value()
 
-			# V_Au0 = 15
-			# V_Au1 = 20
-			# V_Al0 = 22
-			# V_Al1 = 9
-			# V_Deltasz = 30
-
 			Au0 = (float(V_Au0)/100)
 			Au1 = (float(V_Au1)/100)
 			Al0 = (float(V_Al0)/100)
@@ -51,15 +46,23 @@ class Window(QtGui.QDialog):
 
 			cst = CST(x = x,c = 1.,deltasz = [Deltasz/2, Deltasz/2],Au = [Au0, Au1], Al = [Al0,Al1])
 
-			print 'cst:'
-			pprint (cst)
-
 			data1 = [float(cst['l'][k]) for k in range(n)]
 			data2 = [float(cst['u'][k]) for k in range(n)]
 			
 			ax.clear()
-			ax.plot(x, data1, '*-')
-			ax.plot(x, data2, '*-')
+
+			# ax.set_xticks(numpy.arange(0,1, 0.1))
+			# ax.set_yticks(numpy.arange(-1,1,0.005))
+
+			ax.set_xlabel(r"$ \psi$")
+			ax.set_ylabel(r"$ \xi$")
+
+			ax.plot(x, data2, 'r-', label='Upper' )
+			ax.plot(x, data1, 'b-', label='Lower')
+
+			ax.grid(True)
+			ax.legend()
+	
 			self.canvas.draw()
 
 		grid = QtGui.QGridLayout()
@@ -68,8 +71,8 @@ class Window(QtGui.QDialog):
 		self.fig = Figure() 
 		self.canvas = FigureCanvas(self.fig)
 
-		btnR = QtGui.QPushButton('Refresh', self)
-		btnR.clicked.connect(CSTMOD)
+		# btnR = QtGui.QPushButton('Refresh', self)
+		# btnR.clicked.connect(CSTMOD)
 
 		btnQ = QtGui.QPushButton('Quit', self)
 		btnQ.clicked.connect(QtCore.QCoreApplication.instance().quit)
@@ -77,7 +80,7 @@ class Window(QtGui.QDialog):
 		LabelDelta = QtGui.QLabel("Deltasz (x0.001):")
 
 		self.spDelta = QtGui.QSpinBox(self)
-		self.spDelta.setRange(0, 150)
+		self.spDelta.setRange(0, 100)
 		self.spDelta.setSingleStep(1)
 
 		self.slDelta = QtGui.QSlider(QtCore.Qt.Horizontal, self)
@@ -90,12 +93,12 @@ class Window(QtGui.QDialog):
 		LabelAu0 = QtGui.QLabel("Au0 (x0.01):")
 
 		self.spAu0 = QtGui.QSpinBox(self)
-		self.spAu0.setRange(0, 300)
+		self.spAu0.setRange(0, 100)
 		self.spAu0.setSingleStep(1)
 
 		self.slAu0 = QtGui.QSlider(QtCore.Qt.Horizontal, self)
 		self.slAu0.setMinimum(0)
-		self.slAu0.setMaximum(300)
+		self.slAu0.setMaximum(100)
 		self.slAu0.setValue(0)
 		self.slAu0.setTickInterval(10)
 		self.slAu0.setTickPosition(QtGui.QSlider.TicksBelow)
@@ -103,12 +106,12 @@ class Window(QtGui.QDialog):
 		LabelAu1 = QtGui.QLabel("Au1 (x0.01):")
 
 		self.spAu1 = QtGui.QSpinBox(self)
-		self.spAu1.setRange(0, 300)
+		self.spAu1.setRange(0, 100)
 		self.spAu1.setSingleStep(1)
 
 		self.slAu1 = QtGui.QSlider(QtCore.Qt.Horizontal, self)
 		self.slAu1.setMinimum(0)
-		self.slAu1.setMaximum(300)
+		self.slAu1.setMaximum(100)
 		self.slAu1.setValue(0)
 		self.slAu1.setTickInterval(10)
 		self.slAu1.setTickPosition(QtGui.QSlider.TicksBelow)
@@ -116,12 +119,12 @@ class Window(QtGui.QDialog):
 		LabelAl0 = QtGui.QLabel("Al0 (x0.01):")
 
 		self.spAl0 = QtGui.QSpinBox(self)
-		self.spAl0.setRange(0, 300)
+		self.spAl0.setRange(0, 100)
 		self.spAl0.setSingleStep(1)
 
 		self.slAl0 = QtGui.QSlider(QtCore.Qt.Horizontal, self)
 		self.slAl0.setMinimum(0)
-		self.slAl0.setMaximum(300)
+		self.slAl0.setMaximum(100)
 		self.slAl0.setValue(0)
 		self.slAl0.setTickInterval(10)
 		self.slAl0.setTickPosition(QtGui.QSlider.TicksBelow)
@@ -129,12 +132,12 @@ class Window(QtGui.QDialog):
 		LabelAl1 = QtGui.QLabel("Al1 (x0.01):")
 
 		self.spAl1 = QtGui.QSpinBox(self)
-		self.spAl1.setRange(0, 300)
+		self.spAl1.setRange(0, 100)
 		self.spAl1.setSingleStep(1)
 
 		self.slAl1 = QtGui.QSlider(QtCore.Qt.Horizontal, self)
 		self.slAl1.setMinimum(0)
-		self.slAl1.setMaximum(300)
+		self.slAl1.setMaximum(100)
 		self.slAl1.setValue(0)
 		self.slAl1.setTickInterval(10)
 		self.slAl1.setTickPosition(QtGui.QSlider.TicksBelow)
@@ -156,6 +159,21 @@ class Window(QtGui.QDialog):
 		self.slAl1.valueChanged.connect(self.spAl1.setValue)
 		self.spAl1.valueChanged.connect(self.slAl1.setValue)
 
+		self.slDelta.valueChanged.connect(CSTMOD)
+		self.spDelta.valueChanged.connect(CSTMOD)
+
+		self.slAu0.valueChanged.connect(CSTMOD)
+		self.spAu0.valueChanged.connect(CSTMOD)
+
+		self.slAu1.valueChanged.connect(CSTMOD)
+		self.spAu1.valueChanged.connect(CSTMOD)
+
+		self.slAl0.valueChanged.connect(CSTMOD)
+		self.spAl0.valueChanged.connect(CSTMOD)
+
+		self.slAl1.valueChanged.connect(CSTMOD)
+		self.spAl1.valueChanged.connect(CSTMOD)
+
 		grid.addWidget(self.canvas)
 		grid.addWidget(LabelDelta)
 		grid.addWidget(self.spDelta)
@@ -172,7 +190,6 @@ class Window(QtGui.QDialog):
 		grid.addWidget(LabelAl1)
 		grid.addWidget(self.spAl1)
 		grid.addWidget(self.slAl1)
-		grid.addWidget(btnR)
 		grid.addWidget(btnQ)
 		grid.addWidget(self.toolbar)
 
