ebe7fbb9fbec314b463830373b4cbb6e543e44f4
Antoine-Baldo <abaldo@tamu.edu>
2017-10-12 12:56:14 -0500

    Clean up!!

--- GuiMorphing.py
+++ GuiMorphing.py
@@ -55,51 +55,28 @@ class Window(QtGui.QDialog):
 
 			tip_displacement = {'x':.1 + Xt, 'y':1. + Yt}
 
-			# other_points = {'x':[],'y':[]}
+			attrSLX = []
+			attrSLY = []
+			DeltaX = []
+			DeltaY = []
 
-				# other_points['x'].append(DeltaX)
-				# other_points['y'].append(DeltaY)
+			for i in range (n-1):
+				attrSLX.append('slDeltaX' + str(i+2))
+				attrSLY.append('slDeltaY' + str(i+2))
 
-			# for attr in 
+				DeltaX.append(float((getattr(self,attrSLX[i])).value())/100)
+				DeltaY.append(float((getattr(self,attrSLY[i])).value())/100)
+
 			if n >= 2:
-				attrSLX2 = 'slDeltaX' + str(2)
-				attrSLY2 = 'slDeltaY' + str(2)
-				DeltaX2 = (float((getattr(self,attrSLX2)).value())/100)
-				DeltaY2 = (float(getattr(self,attrSLY2).value())/100)
-
-				other_points = {'x': [0.01 + DeltaX2], 'y':[0.1 + DeltaY2]}
-
+				other_points = {'x': [0.01 + DeltaX[0]], 'y': [0.1 + DeltaY[0]]}
 			if n >= 3:
-				attrSLX3 = 'slDeltaX' + str(3)
-				attrSLY3 = 'slDeltaY' + str(3)
-				DeltaX3 = (float((getattr(self,attrSLX3)).value())/100)
-				DeltaY3 = (float(getattr(self,attrSLY3).value())/100)
-
-				other_points = {'x': [DeltaX3, .01 + DeltaX2], 'y':[0.1 + DeltaY3, .3 + DeltaY2]}
-
+				other_points = {'x': [DeltaX[1], .01 + DeltaX[0]], 'y': [0.1 + DeltaY[1], .3 + DeltaY[0]]}
 			if n >= 4:
-				attrSLX4 = 'slDeltaX' + str(4)
-				attrSLY4 = 'slDeltaY' + str(4)
-				DeltaX4 = (float((getattr(self,attrSLX4)).value())/100)
-				DeltaY4 = (float(getattr(self,attrSLY4).value())/100)
-
-				other_points = {'x': [0.01 + DeltaX4, -0.03 + DeltaX3, .05 + DeltaX2], 'y':[0.1 + DeltaY4, 0.3 + DeltaY3, .5 + DeltaY2]}
-
+				other_points = {'x': [0.01 + DeltaX[2], -0.03 + DeltaX[1], .05 + DeltaX[0]], 'y': [0.1 + DeltaY[2], 0.3 + DeltaY[1], .5 + DeltaY[0]]}
 			if n >= 5:
-				attrSLX5 = 'slDeltaX' + str(5)
-				attrSLY5 = 'slDeltaY' + str(5)
-				DeltaX5 = (float((getattr(self,attrSLX5)).value())/100)
-				DeltaY5 = (float(getattr(self,attrSLY5).value())/100)
-
-				other_points = {'x': [0.01 + DeltaX5, -0.03 + DeltaX4, .05 + DeltaX3, 0.12 + DeltaX2], 'y':[0.1 + DeltaY5, 0.3 + DeltaY4, .5 + DeltaY3, 0.8 + DeltaY2]}
-					
+				other_points = {'x': [0.01 + DeltaX[3], -0.03 + DeltaX[2], .05 + DeltaX[1], 0.12 + DeltaX[0]], 'y': [0.1 + DeltaY[3], 0.3 + DeltaY[2], .5 + DeltaY[1], 0.8 + DeltaY[0]]}
 			if n == 6:
-				attrSLX6 = 'slDeltaX' + str(6)
-				attrSLY6 = 'slDeltaY' + str(6)
-				DeltaX6 = (float((getattr(self,attrSLX6)).value())/100)
-				DeltaY6 = (float(getattr(self,attrSLY6).value())/100)
-
-				other_points = {'x': [0.01 + DeltaX6, -0.03 + DeltaX4, .05 + DeltaX3, 0.12 + DeltaX2, 0.01 + DeltaX5], 'y':[0.1 + DeltaY6, 0.3 + DeltaY4, .5 + DeltaY3, 0.8 + DeltaY2, .17 + DeltaY5]}
+				other_points = {'x': [0.01 + DeltaX[4], -0.03 + DeltaX[2], .05 + DeltaX[1], 0.12 + DeltaX[0], 0.01 + DeltaX[3]], 'y': [0.1 + DeltaY[4], 0.3 + DeltaY[2], .5 + DeltaY[1], 0.8 + DeltaY[0], .17 + DeltaY[3]]}
 
 			A0 = -tip_displacement['x']
 			A = calculate_shape_coefficients_tracing(A0, tip_displacement, other_points, 1., 1.)
@@ -160,7 +137,6 @@ class Window(QtGui.QDialog):
 
 			getattr(self,attrSLX).valueChanged.connect(Morphing_Mod)
 			getattr(self,attrSLY).valueChanged.connect(Morphing_Mod)
-
 
 		LabelX= QtGui.QLabel("      X:")
 		LabelY= QtGui.QLabel("      Y:")
