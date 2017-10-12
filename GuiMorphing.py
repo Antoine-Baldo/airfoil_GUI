f0b25a0ba56b85d1b824e199985cff58c7c00020
Antoine-Baldo <abaldo@tamu.edu>
2017-10-12 11:54:30 -0500

    not enough clean up

--- GuiMorphing.py
+++ GuiMorphing.py
@@ -18,10 +18,10 @@ import inspect
 
 def run():
 	app = QtGui.QApplication(sys.argv)
-	n = 4
-	# n = input("Number of points:\n")
-	if n > 6:
-		print "Number of points have to be between 1 and 6."
+	# n = 4
+	n = input("Number of point:\n")
+	if n > 6 or n == 1:
+		print "Number of point have to be between 2 and 6."
 		quit()
 	GUI = Window(n)
 
@@ -50,54 +50,55 @@ class Window(QtGui.QDialog):
 			attrSLX = 'slDeltaX' + str(1)
 			attrSLY = 'slDeltaY' + str(1)
 
-			Xt = getattr(self,attrSLX).value()
-			Yt = getattr(self,attrSLY).value()
+			Xt = float(getattr(self,attrSLX).value())/100
+			Yt = float(getattr(self,attrSLY).value())/100
 
 			tip_displacement = {'x':.1 + Xt, 'y':1. + Yt}
 
 			# other_points = {'x':[],'y':[]}
-
-			for i in range (n-1):
-				attrSLX = 'slDeltaX' + str(i+2)
-				attrSLY = 'slDeltaY' + str(i+2)
-
-				if i == 0:
-					DeltaX2 = (int((getattr(self,attrSLX)).value())/100)
-					DeltaY2 = (int(getattr(self,attrSLY).value())/100)
-
-				if i == 1:
-					DeltaX3 = (int((getattr(self,attrSLX)).value())/100)
-					DeltaY3 = (int(getattr(self,attrSLY).value())/100)
-
-				if i == 2:
-					DeltaX4 = (int((getattr(self,attrSLX)).value())/100)
-					DeltaY4 = (int(getattr(self,attrSLY).value())/100)
-
-				if i == 3:
-					DeltaX5 = (int((getattr(self,attrSLX)).value())/100)
-					DeltaY5 = (int(getattr(self,attrSLY).value())/100)
-
-				if i == 4:
-					DeltaX6 = (int((getattr(self,attrSLX)).value())/100)
-					DeltaY6 = (int(getattr(self,attrSLY).value())/100)
 
 				# other_points['x'].append(DeltaX)
 				# other_points['y'].append(DeltaY)
 
 			# for attr in 
 			if n >= 2:
+				attrSLX2 = 'slDeltaX' + str(2)
+				attrSLY2 = 'slDeltaY' + str(2)
+				DeltaX2 = (float((getattr(self,attrSLX2)).value())/100)
+				DeltaY2 = (float(getattr(self,attrSLY2).value())/100)
+
 				other_points = {'x': [0.01 + DeltaX2], 'y':[0.1 + DeltaY2]}
 
 			if n >= 3:
+				attrSLX3 = 'slDeltaX' + str(3)
+				attrSLY3 = 'slDeltaY' + str(3)
+				DeltaX3 = (float((getattr(self,attrSLX3)).value())/100)
+				DeltaY3 = (float(getattr(self,attrSLY3).value())/100)
+
 				other_points = {'x': [DeltaX3, .01 + DeltaX2], 'y':[0.1 + DeltaY3, .3 + DeltaY2]}
 
 			if n >= 4:
+				attrSLX4 = 'slDeltaX' + str(4)
+				attrSLY4 = 'slDeltaY' + str(4)
+				DeltaX4 = (float((getattr(self,attrSLX4)).value())/100)
+				DeltaY4 = (float(getattr(self,attrSLY4).value())/100)
+
 				other_points = {'x': [0.01 + DeltaX4, -0.03 + DeltaX3, .05 + DeltaX2], 'y':[0.1 + DeltaY4, 0.3 + DeltaY3, .5 + DeltaY2]}
 
 			if n >= 5:
+				attrSLX5 = 'slDeltaX' + str(5)
+				attrSLY5 = 'slDeltaY' + str(5)
+				DeltaX5 = (float((getattr(self,attrSLX5)).value())/100)
+				DeltaY5 = (float(getattr(self,attrSLY5).value())/100)
+
 				other_points = {'x': [0.01 + DeltaX5, -0.03 + DeltaX4, .05 + DeltaX3, 0.12 + DeltaX2], 'y':[0.1 + DeltaY5, 0.3 + DeltaY4, .5 + DeltaY3, 0.8 + DeltaY2]}
 					
 			if n == 6:
+				attrSLX6 = 'slDeltaX' + str(6)
+				attrSLY6 = 'slDeltaY' + str(6)
+				DeltaX6 = (float((getattr(self,attrSLX6)).value())/100)
+				DeltaY6 = (float(getattr(self,attrSLY6).value())/100)
+
 				other_points = {'x': [0.01 + DeltaX6, -0.03 + DeltaX4, .05 + DeltaX3, 0.12 + DeltaX2, 0.01 + DeltaX5], 'y':[0.1 + DeltaY6, 0.3 + DeltaY4, .5 + DeltaY3, 0.8 + DeltaY2, .17 + DeltaY5]}
 
 			A0 = -tip_displacement['x']
@@ -121,29 +122,29 @@ class Window(QtGui.QDialog):
 		for i in range(n):
 			attrSPX = 'spDeltaX'+str(i+1)
 			setattr(self, attrSPX, QtGui.QSpinBox(self))
-			getattr(self,attrSPX).setRange(-100,100)
+			getattr(self,attrSPX).setRange(-50,50)
 			getattr(self,attrSPX).setSingleStep(1)
 
 			attrSPY = 'spDeltaY'+str(i+1)
 			setattr(self, attrSPY, QtGui.QSpinBox(self))
-			getattr(self,attrSPY).setRange(-100,100)
+			getattr(self,attrSPY).setRange(-20,20)
 			getattr(self,attrSPY).setSingleStep(1)
 
 			attrSLX = 'slDeltaX'+str(i+1)
 			setattr(self, attrSLX, QtGui.QSlider(QtCore.Qt.Horizontal, self))
-			getattr(self,attrSLX).setMinimum(-100)
-			getattr(self,attrSLX).setMaximum(100)
+			getattr(self,attrSLX).setMinimum(-50)
+			getattr(self,attrSLX).setMaximum(50)
 			getattr(self,attrSLX).setValue(0)
 			getattr(self,attrSLX).setTickPosition(QtGui.QSlider.TicksBelow)
 			getattr(self,attrSLX).setTickInterval(10)
 
 			attrSLY = 'slDeltaY'+str(i+1)
 			setattr(self, attrSLY, QtGui.QSlider(QtCore.Qt.Vertical, self))
-			getattr(self,attrSLY).setMinimum(-100)
-			getattr(self,attrSLY).setMaximum(100)
+			getattr(self,attrSLY).setMinimum(-20)
+			getattr(self,attrSLY).setMaximum(20)
 			getattr(self,attrSLY).setValue(0)
 			getattr(self,attrSLY).setTickPosition(QtGui.QSlider.TicksBelow)
-			getattr(self,attrSLY).setTickInterval(20)
+			getattr(self,attrSLY).setTickInterval(5)
 
 			grid.addWidget(getattr(self,attrSPX),i+2,2)
 			grid.addWidget(getattr(self,attrSLX),i+2,0)
