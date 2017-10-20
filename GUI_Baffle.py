75e3216ca1ac9874e25d4e8e3dbe91237afb45bc
Antoine-Baldo <abaldo@tamu.edu>
2017-10-20 12:58:16 +0200

    .

--- GUI_Baffle.py
+++ GUI_Baffle.py
@@ -7,22 +7,20 @@ from CST_module import *
 import sys
 import os 
 from pprint import pprint
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.patches as mpatches
 from matplotlib.figure import Figure
 import math
 import numpy as np
 from PyQt4 import QtGui, QtCore
-import inspect
 
 def run():
 	app = QtGui.QApplication(sys.argv)
-	# n = 4
-	n = input("Number of point:\n")
-	if n > 6 or n == 1:
-		print "Number of point have to be between 2 and 6."
-		quit()
+	n = 6
+	# n = input("Number of point:\n")
+	# if n > 6 or n == 1:
+	# 	print "Number of point have to be between 2 and 6."
+	# 	quit()
 	GUI = Window(n)
 
 	GUI.show()
@@ -33,7 +31,7 @@ class Window(QtGui.QDialog):
 	def __init__(self, n, parent=None):
 		super(Window, self).__init__(parent)
 		_fromUtf8 = QtCore.QString.fromUtf8
-		self.setGeometry(400,40,1200,980)
+		self.setGeometry(200,35,1000,750)
 		self.setWindowTitle('Morphing Control')
 		self.setWindowIcon(QtGui.QIcon('images.png'))
 
@@ -47,82 +45,132 @@ class Window(QtGui.QDialog):
 		btnQ.clicked.connect(QtCore.QCoreApplication.instance().quit)
 
 		def Morphing_Mod():
-			attrSLX = 'slDeltaX' + str(1)
-			attrSLY = 'slDeltaY' + str(1)
-
-			Xt = float(getattr(self,attrSLX).value())/100
-			Yt = float(getattr(self,attrSLY).value())/100
-
-			tip_displacement = {'x':.1 + Xt, 'y':1. + Yt}
-
-			attrSLX = []
-			attrSLY = []
-			DX = []
-			DY = []
-
-			for i in range (n-1):
-				attrSLX.append('slDeltaX' + str(i+2))
-				attrSLY.append('slDeltaY' + str(i+2))
-
-				DX.append(float((getattr(self,attrSLX[i])).value())/100)
-				DY.append(float((getattr(self,attrSLY[i])).value())/100)
-
-			if n >= 2:
-				other_points = {'x': [0.01 + DX[0]], 'y': [0.1 + DY[0]]}
-			if n >= 3:
-				other_points = {'x': [DX[1], .01 + DX[0]], 'y': [0.1 + DY[1], .3 + DY[0]]}
-			if n >= 4:
-				other_points = {'x': [0.01 + DX[2], -0.03 + DX[1], .05 + DX[0]], 'y': [0.1 + DY[2], 0.3 + DY[1], .5 + DY[0]]}
-			if n >= 5:
-				other_points = {'x': [0.01 + DX[3], -0.03 + DX[2], .05 + DX[1], 0.12 + DX[0]], 'y': [0.1 + DY[3], 0.3 + DY[2], .5 + DY[1], 0.8 + DY[0]]}
-			if n == 6:
-				other_points = {'x': [0.01 + DX[4], -0.03 + DX[2], .05 + DX[1], 0.12 + DX[0], 0.01 + DX[3]], 'y': [0.08 + DY[4], 0.3 + DY[2], .5 + DY[1], 0.8 + DY[0], .17 + DY[3]]}
-
-			A0 = -tip_displacement['x']
-			A = calculate_shape_coefficients_tracing(A0, tip_displacement, other_points, 1., 1.)
-			y = np.linspace(0, tip_displacement['y'], 100000)
-			x = CST(y, tip_displacement['y'], deltasz= tip_displacement['x'],  Au = A, N1=1., N2=1.)
-
+			n1 = 1.
+			n2 = 1.
+			attrSPX = []
+			attrSPY = []
+			X = []
+			Y = []
 			ax = self.fig.add_subplot(111)
 			ax.clear()
+
+			for i in range (n):
+				attrSPX.append('spX' + str(i+1))
+				attrSPY.append('spY' + str(i+1))
+
+				X.append(float((getattr(self,attrSPX[i])).value())/100)
+				Y.append(float((getattr(self,attrSPY[i])).value())/100)
+
+			tip_displacement = {'x':X[0], 'y':Y[0]}
+
+			if n >= 2:
+				other_points = {'x': [X[1]], 'y': [Y[1]]}
+			if n >= 3:
+				other_points = {'x': [X[2],X[1]], 'y': [Y[2],Y[1]]}
+			if n >= 4:
+				other_points = {'x': [X[3],X[2],X[1]], 'y': [Y[3],Y[2],Y[1]]}
+			if n >= 5:
+				other_points = {'x': [X[4],X[3],X[2],X[1]], 'y': [Y[4],Y[3],Y[2],Y[1]]}
+			if n == 6:
+				other_points = {'x': [X[5],X[4],X[3],X[2],X[1]], 'y': [Y[5],Y[4], Y[3], Y[2],Y[1]]}
+
+			A0 = -tip_displacement['x']
+			print A0
+			A = calculate_shape_coefficients_tracing(A0, other_points['x'], other_points['y'], n1, n2, chord = tip_displacement['y'], EndThickness = tip_displacement['x'])
+
+			#plotting
+			y = np.linspace(0, tip_displacement['y'], 100000)
+			x = CST(y, tip_displacement['y'], deltasz= tip_displacement['x'],  Au = A, N1=n1, N2=n2)
 			ax.plot(x,y)
-			ax.scatter(other_points['x'] + [tip_displacement['x']],other_points['y'] + [tip_displacement['y']])
+			ax.scatter(other_points['x'] + [tip_displacement['x']], 
+			            other_points['y'] + [tip_displacement['y']])
+
 			ax.set_aspect('equal')
 			ax.set_ylim([0,1.1])
 			ax.set_xlim([-0.5,.5])
 			self.canvas.draw()
+###############################################################################################
+			# attrSLX = 'slDeltaX' + str(1)
+			# attrSLY = 'slDeltaY' + str(1)
 
+			# Xt = float(getattr(self,attrSLX).value())/100
+			# Yt = float(getattr(self,attrSLY).value())/100
+
+			# tip_displacement = {'x':.1 + Xt, 'y':1. + Yt}
+
+			# attrSLX = []
+			# attrSLY = []
+			# X = []
+			# Y = []
+
+			# for i in range (n-1):
+			# 	attrSLX.append('slDeltaX' + str(i+2))
+			# 	attrSLY.append('slDeltaY' + str(i+2))
+
+			# 	X.append(float((getattr(self,attrSLX[i])).value())/100)
+			# 	Y.append(float((getattr(self,attrSLY[i])).value())/100)
+
+			# if n >= 2:
+			# 	other_points = {'x': [0.01 + X[0]], 'y': [0.1 + Y[0]]}
+			# if n >= 3:
+			# 	other_points = {'x': [X[1], .01 + X[0]], 'y': [0.1 + Y[1], .3 + Y[0]]}
+			# if n >= 4:
+			# 	other_points = {'x': [0.01 + X[2], -0.03 + X[1], .05 + X[0]], 'y': [0.1 + Y[2], 0.3 + Y[1], .5 + Y[0]]}
+			# if n >= 5:
+			# 	other_points = {'x': [0.01 + X[3], -0.03 + X[2], .05 + X[1], 0.12 + X[0]], 'y': [0.1 + Y[3], 0.3 + Y[2], .5 + Y[1], 0.8 + Y[0]]}
+			# if n == 6:
+			# 	other_points = {'x': [0.01 + X[4], -0.03 + X[2], .05 + X[1], 0.12 + X[0], 0.01 + X[3]], 'y': [0.08 + Y[4], 0.3 + Y[2], .5 + Y[1], 0.8 + Y[0], .17 + Y[3]]}
+
+			# A0 = -tip_displacement['x']
+			# A = calculate_shape_coefficients_tracing(A0, tip_displacement, other_points, 1., 1.)
+			# y = np.linspace(0, tip_displacement['y'], 100000)
+			# x = CST(y, tip_displacement['y'], deltasz= tip_displacement['x'],  Au = A, N1=1., N2=1.)
+
+			# ax = self.fig.add_subplot(111)
+			# ax.clear()
+			# ax.plot(x,y)
+			# ax.scatter(other_points['x'] + [tip_displacement['x']],other_points['y'] + [tip_displacement['y']])
+			# ax.set_aspect('equal')
+			# ax.set_ylim([0,1.1])
+			# ax.set_xlim([-0.5,.5])
+			# self.canvas.draw()
+###########################################################################################################################################
 		btnS = QtGui.QPushButton('Start', self)
 		btnS.clicked.connect(Morphing_Mod)
 
 		grid.addWidget(self.canvas)
 
+		ValX = [10,10 ,25,42,65,90]
+		ValY = [100,2,3,4,5,5]
+
 		for i in range(n):
-			attrSPX = 'spDeltaX'+str(i+1)
+			attrSPX = 'spX'+str(i+1)
 			setattr(self, attrSPX, QtGui.QSpinBox(self))
-			getattr(self,attrSPX).setRange(-25,25)
+			getattr(self,attrSPX).setRange(-100,100)
 			getattr(self,attrSPX).setSingleStep(1)
+			getattr(self,attrSPX).setValue(ValX[i])
 
-			attrSPY = 'spDeltaY'+str(i+1)
+			attrSPY = 'spY'+str(i+1)
 			setattr(self, attrSPY, QtGui.QSpinBox(self))
-			getattr(self,attrSPY).setRange(-20,20)
+			getattr(self,attrSPY).setRange(-100,100)
 			getattr(self,attrSPY).setSingleStep(1)
+			getattr(self,attrSPY).setValue(ValY[i])
 
-			attrSLX = 'slDeltaX'+str(i+1)
+			attrSLX = 'slX'+str(i+1)
 			setattr(self, attrSLX, QtGui.QSlider(QtCore.Qt.Horizontal, self))
-			getattr(self,attrSLX).setMinimum(-25)
-			getattr(self,attrSLX).setMaximum(25)
-			getattr(self,attrSLX).setValue(0)
+			getattr(self,attrSLX).setMinimum(-100)
+			getattr(self,attrSLX).setMaximum(100)
 			getattr(self,attrSLX).setTickPosition(QtGui.QSlider.TicksBelow)
-			getattr(self,attrSLX).setTickInterval(5)
+			getattr(self,attrSLX).setTickInterval(10)
+			getattr(self,attrSLX).setValue(ValX[i])
 
-			attrSLY = 'slDeltaY'+str(i+1)
+			attrSLY = 'slY'+str(i+1)
 			setattr(self, attrSLY, QtGui.QSlider(QtCore.Qt.Vertical, self))
-			getattr(self,attrSLY).setMinimum(-20)
-			getattr(self,attrSLY).setMaximum(20)
-			getattr(self,attrSLY).setValue(0)
+			getattr(self,attrSLY).setMinimum(-100)
+			getattr(self,attrSLY).setMaximum(100)
 			getattr(self,attrSLY).setTickPosition(QtGui.QSlider.TicksBelow)
-			getattr(self,attrSLY).setTickInterval(5)
+			getattr(self,attrSLY).setTickInterval(20)
+			getattr(self,attrSLY).setValue(ValY[i])
 
 			grid.addWidget(getattr(self,attrSPX),i+2,2)
 			grid.addWidget(getattr(self,attrSLX),i+2,0)
--- Gui_Structurally_Consistent_Morphing.py
+++ Gui_Structurally_Consistent_Morphing.py
@@ -25,7 +25,7 @@ class Window(QtGui.QDialog):
 	def __init__(self, parent=None):
 		super(Window, self).__init__(parent)
 		_fromUtf8 = QtCore.QString.fromUtf8
-		self.setGeometry(200,150,1400,680)
+		self.setGeometry(200,50,1100,680)
 		self.setWindowTitle('Structurally Consistent Control')
 		self.setWindowIcon(QtGui.QIcon('images.png'))
 
@@ -58,7 +58,11 @@ class Window(QtGui.QDialog):
 				attrSPpsispar.append('sppsi_spar' + str(i+1))
 
 				V_AC_u.append(float((getattr(self,attrSPACu[i])).value())/100000)
-				V_psi_spar.append(float((getattr(self,attrSPpsispar[i])).value())/10)
+				V_psi_spar.append(float((getattr(self,attrSPpsispar[i])).value())/100)
+
+			print '##############################################################'
+			print 'V_AC_u: ',V_AC_u
+			print 'V_psi_spar: ',V_psi_spar
 
 			AC_u1 = V_AC_u[0]
 			AC_u2 = V_AC_u[1]
@@ -74,9 +78,9 @@ class Window(QtGui.QDialog):
 			psi_spars = [psi_spar1, psi_spar2, psi_spar3, psi_spar4, psi_spar5]
 
 			Au_C, Al_C, c_C, spar_thicknesses = calculate_dependent_shape_coefficients(
-	                                                            AC_u1, AC_u2, AC_u3, AC_u4, AC_u5,
-	                                                            psi_spars, Au_P, Al_P,
-	                                                            deltaz, c_P, morphing=morphing_direction)
+                                                            V_AC_u,
+                                                            psi_spars, Au_P, Al_P,
+                                                            deltaz, c_P, morphing=morphing_direction)
 			np.set_printoptions(precision=20)
 
 			ax = self.fig.add_subplot(111)
@@ -113,7 +117,7 @@ class Window(QtGui.QDialog):
 				s = calculate_spar_direction(psi_spars[j], Au_P, Au_C, deltaz, c_C)
 
 				# Print spars for children
-				ax.plot([x_children_j, x_children_j - spar_thicknesses[j]*s[0]],[-y_children_j, -y_children_j + spar_thicknesses[j]*s[1]], 'b', lw=2, label=None)
+				ax.plot([x_children_j, x_children_j - spar_thicknesses[j]*s[0]],[y_children_j, y_children_j - spar_thicknesses[j]*s[1]], c = 'b', lw=2, label=None)
 				psi_flats.append(x_children_j - spar_thicknesses[j]*s[0])
 				y = CST(np.array([psi_parent_j*c_P]), c_P, deltasz=[deltaz/2., deltaz/2.], Al= Al_P, Au =Au_P)
 
@@ -121,7 +125,7 @@ class Window(QtGui.QDialog):
 				intersections_y_children.append(y_children_j - spar_thicknesses[j]*s[1])
 
 				# Print spars for parents
-				ax.plot([psi_parent_j*c_P, psi_parent_j*c_P], [-y['u'], -y['u']+spar_thicknesses[j]], 'r--', lw=2, label = None)
+				ax.plot([psi_parent_j*c_P, psi_parent_j*c_P], [y['u'], y['u']-spar_thicknesses[j]], 'r--', lw=2, label = None)
 
 				intersections_x_parent.append(psi_parent_j*c_P)
 				intersections_y_parent.append(y['u']-spar_thicknesses[j])
@@ -134,46 +138,11 @@ class Window(QtGui.QDialog):
 			ax.legend(loc=1)
 			self.canvas.draw()
 
-			print c_C, c_P
-			# Calculate initial lengths
-			psi_list = [0.] + psi_spars + [c_P]
-			print psi_list
-			initial_lengths = []
-			for i in range(len(psi_list)-1):
-				initial_lengths.append(calculate_arc_length(psi_list[i], psi_list[i+1], Al_P, deltaz, c_P))
-			# Calculate final lengths
-			final_lengths = []
-			psi_list = [0.] + psi_flats + [c_C] # In P configuration
-			print psi_list
-			for i in range(len(psi_list)-1):
-				print psi_list[i]*c_P/c_C, psi_list[i+1]*c_P/c_C
-				final_lengths.append(calculate_arc_length(psi_list[i]*c_P/c_C, psi_list[i+1]*c_P/c_C, Al_C, deltaz, c_C))
-			# Calculate strains
-			strains = []
-			for i in range(len(final_lengths)):
-				strains.append((final_lengths[i]-initial_lengths[i])/initial_lengths[i])
-            
-			for i in range(len(strains)):
-				print 'Initial length: ' + str(initial_lengths[i]) + ', final length: ' + str(final_lengths[i]) + ', strains: ' + str(strains[i])
-                
-			intersections_x_children.append(c_C)
-			intersections_y_children.append(0)
-			intersections_x_parent.append(c_P)
-			intersections_y_parent.append(0)		
-			# Wire lengths
-			for i in range(len(intersections_x_children)-1):
-				length_parent = math.sqrt((intersections_x_parent[i]-intersections_x_parent[i+1])**2+
-                                          (intersections_y_parent[i]-intersections_y_parent[i+1])**2)
-				length_children = math.sqrt((intersections_x_children[i]-intersections_x_children[i+1])**2+
-                                            (intersections_y_children[i]-intersections_y_children[i+1])**2)
-				print (length_children-length_parent)/length_parent
-
-
 		btnS = QtGui.QPushButton('Start', self)
 		btnS.clicked.connect(structurally_consistent)
 
 		ValACu = [14870 ,10843,15084,10919,12840]
-		Valpsispar = [2,3,5,7,9]
+		Valpsispar = [20,30,50,70,90]
 
 		for i in range (5):
 			attrSPACu = 'spAC_u'+str(i+1)
@@ -184,7 +153,7 @@ class Window(QtGui.QDialog):
 
 			attrSPpsispar = 'sppsi_spar'+str(i+1)
 			setattr(self, attrSPpsispar, QtGui.QSpinBox(self))
-			getattr(self,attrSPpsispar).setRange(0,10)
+			getattr(self,attrSPpsispar).setRange(0,100)
 			getattr(self,attrSPpsispar).setSingleStep(1)
 			getattr(self,attrSPpsispar).setValue(Valpsispar[i])
 
